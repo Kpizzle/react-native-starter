736939bcfe89ddd46151891f3b9453781a0ef40a
Kyle Potter <kp1289@gmail.com>
2020-11-07 18:32:46 +0000

    init commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const HomeScreen = () => {
+  const name = 'Kyle';
+  const title = (
+    <Text style={styles.text}>Getting started with react native!</Text>
+  );
+  const introduction = <Text style={styles.intro}>My Name is {name}</Text>;
+
+  return (
+    <View>
+      {title}
+      {introduction}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25,
+  },
+  intro: {
+    fontSize: 15,
+  },
+});
+
+export default HomeScreen;
