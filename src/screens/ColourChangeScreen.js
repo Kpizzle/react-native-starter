6438b115acf13b283395102df5fbf9aec54ddaca
Kyle Potter <kp1289@gmail.com>
2020-11-08 18:46:45 +0000

    new component for colours

--- src/screens/ColourChangeScreen.js
+++ src/screens/ColourChangeScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ColourCounter from '../components/ColourCounter'
 
 const ColourChangeScreen = () => {
-  return <Text>New Screen</Text>;
+  return (
+    <View>
+      <ColourCounter colour='Red' />
+      <ColourCounter colour='Blue' />
+      <ColourCounter colour='Green' />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
