fee26f70a1c2857425bdddecdfe73a38fa27a18d
Kyle Potter <kp1289@gmail.com>
2020-11-08 16:36:43 +0000

    added state and reset button in counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-native';
 import { View, Text, StyleSheet } from 'react-native';
 
 const CounterScreen = () => {
-  return <Text>Current Count: </Text>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <View>
+      <Button
+        title='Increase'
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter} </Text>
+      <Button
+        title='Reset'
+        onPress={() => {
+          setCounter(0);
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
