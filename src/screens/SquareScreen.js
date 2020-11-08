68441eafa5504039ff16d8691c3d1651e598c4ce
Kyle Potter <kp1289@gmail.com>
2020-11-08 19:04:25 +0000

    using state between components

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import ColourCounter from '../components/ColourCounter'
+import ColourCounter from '../components/ColourCounter';
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
-      <ColourCounter colour='Red' />
-      <ColourCounter colour='Blue' />
-      <ColourCounter colour='Green' />
+      <ColourCounter
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+        colour='Red'
+      />
+      <ColourCounter
+        onIncrease={() => setBlue(blue + 1)}
+        onDecrease={() => setBlue(blue - 1)}
+        colour='Blue'
+      />
+      <ColourCounter
+        onIncrease={() => setGreen(green + 1)}
+        onDecrease={() => setGreen(green - 1)}
+        colour='Green'
+      />
     </View>
   );
 };
